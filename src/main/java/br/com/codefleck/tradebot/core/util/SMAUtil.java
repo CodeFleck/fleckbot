6f12e31cf31842baa411ed24f20339fd7a75f98b
Daniel Fleck <danielfleck@live.ca>
2018-07-10 09:18:23 -0300

    Remove unnecessary dependencies and delete an obsolete class.

--- src/main/java/br/com/codefleck/tradebot/core/util/SMAUtil.java
+++ src/main/java/br/com/codefleck/tradebot/core/util/SMAUtil.java
@@ -3,11 +3,6 @@ package br.com.codefleck.tradebot.core.util;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import org.ta4j.core.BaseTimeSeries;
 import org.ta4j.core.indicators.SMAIndicator;
 import org.ta4j.core.indicators.helpers.MedianPriceIndicator;
