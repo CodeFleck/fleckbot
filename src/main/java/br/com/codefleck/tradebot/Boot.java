fd54565db8aeaafcd00407349995639ca1c9afe3
Daniel Fleck <danielfleck@live.ca>
2018-07-16 13:13:23 -0300

    remove spring boot dependency

--- src/main/java/br/com/codefleck/tradebot/Boot.java
+++ src/main/java/br/com/codefleck/tradebot/Boot.java
@@ -1,14 +1,14 @@
-package br.com.codefleck.tradebot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Boot
-{
-
-   public static void main(String[] args)
-   {
-      SpringApplication.run(Boot.class, args);
-   }
-}
+//package br.com.codefleck.tradebot;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Boot
+//{
+//
+//   public static void main(String[] args)
+//   {
+//      SpringApplication.run(Boot.class, args);
+//   }
+//}
